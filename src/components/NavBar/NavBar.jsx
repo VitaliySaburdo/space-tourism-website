8b090e505ffec836aff5d25924534b50647e1672
VitaliySaburdo <vsaburdo@gmail.com>
2024-08-07 19:43:02 +0300

    start

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,26 +2,42 @@ import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 export const NavBar = () => {
+  const getNavLinkClass = (isActive) => {
+    return isActive ? `${styles.nav} ${styles.active}` : styles.nav;
+  };
+
   return (
     <>
       <ul className={styles.container}>
         <li className="">
-          <NavLink className="" to="/">
+          <NavLink
+            className={({ isActive }) => getNavLinkClass(isActive)}
+            to="/"
+          >
             00 HOME
           </NavLink>
         </li>
         <li className="">
-          <NavLink className="" to="/destination">
+          <NavLink
+            className={({ isActive }) => getNavLinkClass(isActive)}
+            to="/destination"
+          >
             01 DESTINATION
           </NavLink>
         </li>
         <li className="">
-          <NavLink className="" to="/crew">
+          <NavLink
+            className={({ isActive }) => getNavLinkClass(isActive)}
+            to="/crew"
+          >
             02 CREW
           </NavLink>
         </li>
         <li className="">
-          <NavLink className="" to="/technology">
+          <NavLink
+            className={({ isActive }) => getNavLinkClass(isActive)}
+            to="/technology"
+          >
             03 TECHNOLOGY
           </NavLink>
         </li>
