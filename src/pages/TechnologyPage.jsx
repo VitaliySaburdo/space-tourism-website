a26e5f10de3525dbb9b026f16f7445999b4a6091
VitaliySaburdo <vsaburdo@gmail.com>
2024-08-08 23:23:42 +0300

    fix

--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { Container } from '../components/Container';
 
 export const TechnologyPage = () => {
   return (
     <>
       <Container>
-        <p style={{ color: 'var(--wite)' }}> Technology</p>
+        <p> Technology</p>
       </Container>
     </>
   );
-}
+};
