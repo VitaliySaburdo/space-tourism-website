4402f91ae28d0c3160699c2b5b8be266be9a3d40
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-08-08 11:52:41 +0300

    finally desktop home

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,36 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Header } from '../Header';
+import style from './SharedLayout.module.scss';
 
 export const SharedLayout = () => {
+  const location = useLocation();
+
+  const getBackgroundImage = () => {
+    switch (location.pathname) {
+      case '/':
+        return 'url("/src/assets/images/home/background-home-desktop.jpg")';
+      case '/destination':
+        return 'url("/src/assets/images/destination/background-destination-desktop.jpg")';
+      case '/crew':
+        return 'url("/src/assets/images/crew/background-crew-desktop.jpg")';
+      case '/technology':
+        return 'url("/src/assets/images/technology/background-technology-desktop.jpg")';
+      default:
+        return 'none';
+    }
+  };
+
+  const backgroundImage = getBackgroundImage();
+
   return (
     <>
       <Header />
       <Suspense fallback={null}>
-        <main>
+        <main
+          className={style.main}
+          style={{ backgroundImage: backgroundImage }}
+        >
           <Outlet />
         </main>
       </Suspense>
