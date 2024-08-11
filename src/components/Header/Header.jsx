43544d83b44d97aa3493e7960063500ed2ce0e6b
VitaliySaburdo <vsaburdo@gmail.com>
2024-08-11 15:21:45 +0300

    mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,31 @@
 import { Container } from '../Container';
 import Logo from '/assets/images/shared/logo.svg';
+import MenuOpen from '/assets/images/shared/icon-hamburger.svg';
+import MenuClose from '/assets/images/shared/icon-close.svg';
 import { NavBar } from '../NavBar';
 import style from './Header.module.scss';
+import { useState } from 'react';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <header className={style.header}>
         <Container>
           <div className={style.shell}>
             <div className={style.container}>
-              <img
-                className={style.img}
-                src={Logo}
-                alt="logo"
-                width={48}
-                height={48}
-              />
+              <img className={style.img} src={Logo} alt="logo" />
+              <button onClick={toggleMenu} className={style.btn}>
+                {isMenuOpen ? (
+                  <img className={style.menuOpen} src={MenuOpen} alt="menu" />
+                ) : (
+                  <img className={style.menuClose} src={MenuClose} alt="menu" />
+                )}
+              </button>
               <div className={style.decor}></div>
             </div>
             <NavBar />
