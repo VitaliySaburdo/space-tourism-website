a927e9881e38bdf7cd715fe09b976fc2878876da
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-08-13 11:40:30 +0300

    setInterval Technology

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import style from './Technology.module.scss';
 import { AnimatedText, StaticAnimate } from '../Animate';
 
 export const Technology = ({ data }) => {
   const [currentIdx, setCurrentIdx] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-  const HandleOnClick = (idx) => {
+  const HandleOnClick = idx => {
     setCurrentIdx(idx);
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
   };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIdx(prevIdx => (prevIdx + 1) % data.length);
+    }, 6000);
+    setIntervalId(intervalId);
+    return () => clearInterval(intervalId);
+  }, [data.length]);
+
   return (
     <div className={style.container}>
       <h3 className={style.title}>
@@ -22,9 +36,7 @@ export const Technology = ({ data }) => {
                 return (
                   <li key={idx}>
                     <button
-                      className={`${style.btn} ${
-                        currentIdx === idx ? style.active : ''
-                      }`}
+                      className={`${style.btn} ${currentIdx === idx ? style.active : ''}`}
                       onClick={() => {
                         HandleOnClick(idx);
                       }}
@@ -44,20 +56,14 @@ export const Technology = ({ data }) => {
                   <h2 className={style.name}>{data[currentIdx].name}</h2>
                 </AnimatedText>
                 <AnimatedText delay={0.6}>
-                  <p className={style.description}>
-                    {data[currentIdx].description}
-                  </p>
+                  <p className={style.description}>{data[currentIdx].description}</p>
                 </AnimatedText>
               </AnimatedText>
             </div>
           </div>
           <StaticAnimate key={currentIdx}>
             <div className={style.imgContainer}>
-              <img
-                className={style.img}
-                src={data[currentIdx].images.portrait}
-                alt={data.name}
-              />
+              <img className={style.img} src={data[currentIdx].images.portrait} alt={data.name} />
             </div>
           </StaticAnimate>
         </div>
